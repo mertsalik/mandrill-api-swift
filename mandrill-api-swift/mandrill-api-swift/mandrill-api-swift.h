f85102e38b56befe9d5ce66743edb67a4ee5d931
Mert Salık <mertsalik.itu@gmail.com>
2017-07-17 16:15:44 +0300

    add

---
+++ mandrill-api-swift/mandrill-api-swift/mandrill-api-swift.h
@@ -0,0 +1,19 @@
+//
+//  mandrill-api-swift.h
+//  mandrill-api-swift
+//
+//  Created by Mert Salik on 17/07/17.
+//  Copyright © 2017 Mert Salik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mandrill-api-swift.
+FOUNDATION_EXPORT double mandrill_api_swiftVersionNumber;
+
+//! Project version string for mandrill-api-swift.
+FOUNDATION_EXPORT const unsigned char mandrill_api_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mandrill_api_swift/PublicHeader.h>
+
+
