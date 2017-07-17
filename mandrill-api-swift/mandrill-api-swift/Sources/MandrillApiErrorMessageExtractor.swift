150d4f27921391e416ac802b12320516ee737506
Mert Salık <mertsalik.itu@gmail.com>
2017-07-17 20:33:02 +0300

    mandrill api error message extractor for siesta pipelining

--- mandrill-api-swift/mandrill-api-swift/Sources/MandrillApiErrorMessageExtractor.swift
+++ mandrill-api-swift/mandrill-api-swift/Sources/MandrillApiErrorMessageExtractor.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import Siesta
+import SwiftyJSON
+
+
+struct MandrillApiErrorMessageExtractor: ResponseTransformer{
+    func process(_ response: Response) -> Response {
+        switch response {
+        case .success:
+            return response
+        case .failure(var error):
+            guard let entity = error.entity,
+                let jsonEntity = (entity.content as? JSON)  else {
+                    return .failure(error)
+            }
+            error.entity!.content = MandrillErrorResponse(json: jsonEntity)
+            return .failure(error)
+        }
+    }
+}
